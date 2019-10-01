4714355da4395558ac85a5c4ea4689ea266d3e90
John McCreight <dalrain@dalrain.net>
2019-09-30 21:48:35 -0700

    Actually connect to some things in netmiko testing

--- week1/netmiko-test.py
+++ week1/netmiko-test.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+from getpass import getpass
+from netmiko import ConnectHandler
+
+net_connect = ConnectHandler(
+    host='nxos1.lasthop.io',
+    username='pyclass',
+    password=getpass(),
+    device_type='cisco_nxos',
+    session_log='session_log.log')
+print(net_connect.find_prompt())
