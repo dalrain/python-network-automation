b0fc7550310930cc644ac3379f70abe4dd2b4a04
John McCreight <dalrain@dalrain.net>
2019-09-30 22:18:05 -0700

    Week 1 code as requested

--- week1/netmiko-test.py
+++ week1/netmiko-test.py
@@ -3,10 +3,28 @@
 from getpass import getpass
 from netmiko import ConnectHandler
 
-net_connect = ConnectHandler(
-    host='nxos1.lasthop.io',
-    username='pyclass',
-    password=getpass(),
-    device_type='cisco_nxos',
-    session_log='session_log.log')
-print(net_connect.find_prompt())
+lab_password = getpass()
+
+devices = [{ 
+    'host':'nxos1.lasthop.io',
+    'username':'pyclass',
+    'password':lab_password,
+    'device_type':'cisco_nxos',
+    'session_log':'session_log.log'},
+    {
+    'host':'nxos2.lasthop.io',
+    'username':'pyclass',
+    'password':lab_password,
+    'device_type':'cisco_nxos',
+    'session_log':'session_log.log'},    
+    ]
+
+for device in devices:
+    net_connect = ConnectHandler(**device)
+    print(net_connect.find_prompt())
+    output = net_connect.send_command("show ip int bri vrf all")
+    print(output)
+
+with open("show_version.log","w") as f:
+    f.writelines(net_connect.send_command("show version"))
+    
