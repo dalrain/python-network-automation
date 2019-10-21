3230e76fbe9b78e50a0790f9394d7d1498bd8959
John McCreight <dalrain@dalrain.net>
2019-10-21 14:31:31 -0700

    Adding module

--- week2/netmiko_dict_template.py
+++ week2/netmiko_dict_template.py
@@ -76,5 +76,4 @@ devices = [
 for device in devices[7:9]:
     net_connect = ConnectHandler(**device)
     print(net_connect.find_prompt())
-
-    
+    net_connect.disconnect()    
---
+++ week2/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env/python
+
+import netmiko_dict_template
