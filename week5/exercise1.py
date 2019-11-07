65b2b28abd32f7359c3bd9a4b41e0ac2d7e46416
John McCreight <dalrain@dalrain.net>
2019-11-07 14:31:31 -0800

    Week 5 exercise 1 and 2

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -1,11 +1,23 @@
 from jinja2 import Template
 
 my_template = """
-text
+router bgp {{local_as}}
+  neighbor {{peer1_ip}} remote-as {{peer1_as}}
+    update-source loopback99
+    ebgp-multihop 2
+    address-family ipv4 unicast
+  neighbor {{peer2_ip}} remote-as {{peer2_as}}
+    address-family ipv4 unicast
 """
 
 bgp_vars = {
-    "as_number" : 42,
-    }
+    "local_as" : 42,
+    "peer1_ip" : "10.1.20.2",
+    "peer1_as" : 20,
+    "peer2_ip" : "10.1.30.2",
+    "peer2_as" : 30,
+}
 
+template_object = Template(my_template)
 
+print(template_object.render(**bgp_vars))
---
+++ week5/exercise2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python
+
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader(".")
+
+
+template_file = "exercise1.j2"
+template = env.get_template(template_file)
+output = template.render(**template_vars)
+
+print(output)
