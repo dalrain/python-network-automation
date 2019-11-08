a20bc7b9153f675e78ff11afb633ccfd3d96ca5d
John McCreight <dalrain@dalrain.net>
2019-11-07 16:06:33 -0800

    Exercise 2 updates

--- week5/exercise2a.py
+++ week5/exercise2a.py
@@ -6,8 +6,28 @@ from jinja2.environment import Environment
 env = Environment(undefined=StrictUndefined)
 env.loader = FileSystemLoader(".")
 
+template_vars = {
+    "device" : {
+        "nxos1" : {
+            "interfaces" : {
+                "Ethernet1/1" : {
+                    "ip_address" : "10.100.1.1",
+                    "netmask" : 24,
+                }
+            }
+        },
+        "nxos2" : { 
+            "interfaces" : { 
+                "Ethernet1/1" : { 
+                    "ip_address" : "10.100.1.2",
+                    "netmask" : 24, 
+                }   
+            }   
+        },
+    }
+}
 
-template_file = "exercise1.j2"
+template_file = "exercise2a.j2"
 template = env.get_template(template_file)
 output = template.render(**template_vars)
 
