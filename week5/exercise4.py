39d11870eacc731f6fe6ad5b6a995a7313ae9d55
John McCreight <dalrain@dalrain.net>
2019-11-11 13:16:55 -0800

    Add exercise 3-4

---
+++ week5/exercise4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env/python
+
+from jinja2 import FileSystemLoader, StrictUndefined
+from jinja2.environment import Environment
+
+env = Environment(undefined=StrictUndefined)
+env.loader = FileSystemLoader(".")
+
+template_vars = {
+    "vrfs" : {
+        "blue" : {
+            "rd" : "100:1",
+             "ipv4_enabled" : True,
+             "ipv6_enabled" : True,
+        },
+        "red" : { 
+            "rd" : "200:1",
+             "ipv4_enabled" : False,
+             "ipv6_enabled" : True,
+        },
+    }
+}
+
+template_file = "exercise3-4.j2"
+template = env.get_template(template_file)
+output = template.render(**template_vars)
+
+print(output)
