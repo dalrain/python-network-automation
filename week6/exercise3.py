45856389272c0c7bbcbf2638dfec2aa8988b276d
John McCreight <dalrain@dalrain.net>
2019-12-02 15:11:36 -0800

    Fixed exercise 3

---
+++ week6/exercise3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env/python
+
+import pyeapi
+from getpass import getpass
+from pprint import pprint
+from my_func import load_devices_from_yaml, output_printer
+
+
+device_dict = load_devices_from_yaml()
+device_password = getpass()
+
+for item, device_detail in device_dict.items():
+    device_detail["password"] = device_password
+
+    # This takes transport, host, username, password, and port
+    connection = pyeapi.client.connect(**device_detail)
+
+    #This takes an existing connection and creates a Node instance
+    device = pyeapi.client.Node(connection)
+
+    output = device.enable("show ip route")
+
+    #Debug
+    pprint(output)
+
+    for route, route_attribs in output[0]["result"]["vrfs"]["default"]["routes"].items():
+        pprint(route)
+        pprint(route_attribs["routeType"])
+        if route_attribs["routeType"] == "static":
+            for vias in route_attribs["vias"]:
+                pprint("Next hop: " + vias["nexthopAddr"])
+        pprint("---- Next route ----")
