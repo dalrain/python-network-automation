c214c0c21e3db7caeb53cb8be23239520bc1a82f
John McCreight <dalrain@dalrain.net>
2020-02-09 12:54:57 -0800

    Add exercise 5/6

---
+++ bonus1/exercise6.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+import requests
+import os
+import json
+
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+BASE_URL = "https://netbox.lasthop.io/api/"
+#This will take the addr ID, used for the URL to access the IP object directly
+ADDRESS_ID = input("Enter Address ID that was created in exercise4: ")
+
+if __name__ == "__main__":
+
+    token = os.environ["NETBOX_TOKEN"]
+
+    #This is supposed to use proper headers, in which we send that we're looking for a JSON response
+    #For a POST, to put data on the server, we also need "Content-Type" to tell the server what we're sending
+    http_headers = {}
+    http_headers["Content-Type"] = "application/json; version=2.4;"
+    http_headers["accept"] = "application/json; version=2.4;"
+    http_headers["Authorization"] = f"Token {token}"
+
+    #Just delete the URL provided, no data needed
+    print("Deleting the specified address ID")
+    response = requests.delete(
+        f"{BASE_URL}ipam/ip-addresses/{ADDRESS_ID}/",
+        headers=http_headers,
+        verify=False,
+    )
+
+    print("Sending the DELETE data to remove the IP addr object:")
+    print(f"It returned response code {response.status_code}")
+
+
+    
+
